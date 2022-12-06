a296fa4c194809cdca99f7a947f79b45d9717dd7
MatSung <108081939+MatSung@users.noreply.github.com>
2022-12-06 20:04:05 +0200

    funkcija

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-let amount = 0;
-let answer = "";
-if(amount <= 0){
-    answer = "ne grupė";
-} else if(amount == 1){
-    answer = "solo"
-} else if(amount == 2){
-    answer = "duetas";
-}else if(amount == 3){
-    answer = "trio";
-} else if (amount == 4){
-    answer = "kvartetas";
-} else{
-    answer = "didelė grupė";
-} 
-console.log(answer);
+
+
+checkAge(2);
+
+function checkAge(age){
+    return (age > 18) ? true : confirm("did parents allow you?");
+}
+
+function confirm(message){
+    console.log(message);
+}
