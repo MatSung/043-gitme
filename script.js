4f0ca7eeacd05cebf9405fc9d5ad8a670b15b394
MatSung <108081939+MatSung@users.noreply.github.com>
2022-12-20 20:48:54 +0200

    Name from userid

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 // arašykite funkciją getPosts, kuri gražina Promise, kuriame yra išgaunami post'ai iš https://jsonplaceholder.typicode.com/posts panaudojant vieną iš galimų AJAX būdų: XMLHttpRequest, fetch, jQuery, axios. Užsibaigus Promise atvaizduokite gautus duomenys HTML lentelėje.
 
 
-function getPosts(url) {
+function getData(url) {
     return fetch(url)
         .then((response) => response.json())
     //.then((data) => {console.log(data);});
 }
 
-function fillTable(data) {
+
+function fillTable(data, users) {
     let table = document.getElementById("table");
     let tableHeadRow = document.createElement("tr");
     let objectKeys = Object.keys(data[0]);
-    
+
+    // prideti Name prie heade
+    let nameOfPosterHead = document.createElement("th");
+    nameOfPosterHead.textContent = "Name";
+    tableHeadRow.append(nameOfPosterHead);
+    ///////////////////////////////////
+
     objectKeys.forEach(element => {
         let newHeadCell = document.createElement("th");
         newHeadCell.textContent = element;
         tableHeadRow.append(newHeadCell);
     });
     table.append(tableHeadRow);
-
     data.forEach(element => {
         let newRow = document.createElement("tr");
+
+        // prideti varda prie kiekvieno posto
+        let nameTd = document.createElement("td");
+        nameTd.textContent = users[element.userId];
+        newRow.append(nameTd);
+        ///////////////////////////////////
+
         for (const key in element) {
             if (Object.hasOwnProperty.call(element, key)) {
                 const item = element[key];
@@ -33,4 +46,20 @@ function fillTable(data) {
     });
 }
 
-getPosts('https://jsonplaceholder.typicode.com/posts').then(data => { fillTable(data) });
+function getUsers(data) {
+    let users = {};
+    data.forEach((element, index) => {
+        users[element.id] = element.name;
+    });
+    return users;
+}
+
+getData("https://jsonplaceholder.typicode.com/users")
+    .then(data => getUsers(data))
+    .then(users => {
+        getData('https://jsonplaceholder.typicode.com/posts')
+            .then(data => { fillTable(data,users) });
+    })
+
+
+//Papildikite prieš tai darytą užduotį: panaudojant gautą userId parodykite vartotojo vardą šalia "post" informacijos.
